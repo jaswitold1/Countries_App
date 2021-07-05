09b9229705a266d6bda51debab640dbc098e0087
Witold Jas <jaswitold1@gmail.com>
2021-07-05 20:09:05 +0200

    styles added for continents tiles

--- src/components/Continents.tsx
+++ src/components/Continents.tsx
@@ -10,15 +10,18 @@ function Continents() {
     
     
     return (
-        
-      continents ?  continents?.map((el:any,i:number)=>{
-            return <Link key={i} to={`/continents/${el.code}`}>
-                        {
-                            el.name
-                        }
+        <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+            {
+            continents ?  continents?.map((el:any,i:number)=>{
+                return <Link style={{display:'flex',flexDirection:'column',alignItems:'center',textDecoration:'none', backgroundColor:'yellow', border:"1px solid black", width:'30vw', margin:'5px' }} key={i} to={`/continents/${el.code}`}>
+                        <h1 style={{textDecoration:'none'}}>{el.name}</h1>
+                        <h2 style={{textDecoration:'none'}}>{el.code}</h2>
                     </Link>
                     
-        }) : "loading"
+                })  : "loading"
+            }
+        </div>
+   
       
     )
 }

c3def0fb4193351c99062540bb859da4e1597304
Witold Jas <jaswitold1@gmail.com>
2021-07-05 21:56:22 +0200

    front page flamboyant styles

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,8 +4,15 @@ import {Link as LinkRouter} from 'react-router-dom'
 
 function Link() {
     return (
-        <LinkRouter to='/continents' >
-           link
+        <LinkRouter style={{
+            display:'flex',
+            justifyContent:'center',
+             alignItems:'center',
+             height:'100vh',
+             textDecoration:'none',
+             color: 'coral'
+             }} to='/continents' >
+           <h1>Link</h1>
         </LinkRouter>
     )
 }
